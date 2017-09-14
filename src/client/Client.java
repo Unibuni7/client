67c58248ef30b1a84a8808f748da45c7d1de700f
Gitgest <aable24@gmail.com>
2017-09-14 11:25:23 +0200

    Changes made
    Added Server.java, ClientConnection.java, fixed Client.java

--- src/client/Client.java
+++ src/client/Client.java
@@ -9,27 +9,41 @@ public class Client {
     public static void main(String[] args)
     {
         try {
-            Socket s = new Socket("127.0.0.1",8001);
+            Socket s = new Socket("127.0.0.1", 8001);
 
-            while(true)
-            {
+            while (true) {
                 InputStream input = s.getInputStream();
                 OutputStream output = s.getOutputStream();
 
                 Scanner in = new Scanner(input);
 
-                PrintWriter out = new PrintWriter(output,true);
+                PrintWriter out = new PrintWriter(output, true);
 
+                String welcome = in.nextLine();
                 out.println("Velkommen");
 
-                boolean done = false;
-                while()
+                //nu kan vi så sende og modtage respektivt
+                out.println("Først besked");
+                // Vi udksriver nu bare direkte
+                System.out.println(in.nextLine());
+
+                out.println("Anden besked");
+                // Vi udskriver nu bare direkte
+                System.out.println(in.nextLine());
+
+                out.println("Tredie besked");
+                // Vi udksriver nu bare direkte
+                System.out.println(in.nextLine());
+
+                //Denne gang lukker vi selv forbindelsen. Fordi vi kan.
+                //Vi kunne også have brugt "luk ned"
+                s.close();
+                System.out.println("Forbindelsen lukket.");
 
             }
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+        }
+        catch (IOException ex) {
+            // We ignore this.
         }
     }
 }
---
+++ src/server/Server.java
@@ -0,0 +1,38 @@
+package server;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.*;
+import java.util.Scanner;
+
+public class Server {
+
+    public static void main(String[] args){
+
+        ServerSocket ss;
+        try
+        {
+        //ServerSocket oprettes og port 8001 angives som den der skal lyttes på
+        ss = new ServerSocket(8001);
+
+        System.out.println("Server kører...");
+        while(true) {
+            // så længe der ikke er oprettet en forbindelse, venster serveren her
+            //så snart der anmodes om en forbindelse accepteres den med accept()
+            Socket incoming = ss.accept();
+            System.out.println("Klient forbundet");
+
+            Runnable r = new ClientConnection(incoming);
+            Thread t = new Thread(r);
+            t.start();
+        }
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+
+}
